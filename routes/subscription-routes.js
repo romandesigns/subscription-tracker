7e26c7eca8c0d51722579ed4f7d66b91c80d6030
Roman <wavystyledev@gmail.com>
2025-02-09 16:00:54 -0500

    implemented get user subscriptions and create subscriptions

--- routes/subscription-routes.js
+++ routes/subscription-routes.js
@@ -1,13 +1,15 @@
 import {Router} from 'express';
+import authorize from "../middleware/auth-middleware.js";
+import {createSubscription, getUserSubscriptions} from "../controllers/subscription-controller.js";
 
 const subscriptionRoutes = Router();
 
 subscriptionRoutes.get('/',  (req, res) => res.send({title:'Get all subscriptions'}));
 subscriptionRoutes.get('/:id',  (req, res) => res.send({title:'Get subscription details'}));
-subscriptionRoutes.post('/',  (req, res) => res.send({title:'Create a new subscription'}));
+subscriptionRoutes.post('/', authorize,createSubscription);
 subscriptionRoutes.put('/:id',  (req, res) => res.send({title:'Update subscription by ID'}));
 subscriptionRoutes.delete('/:id',  (req, res) => res.send({title:'Delete subscription by ID'}));
-subscriptionRoutes.get('/user/:id',  (req, res) => res.send({title:'Get all users subscription'}));
+subscriptionRoutes.get('/user/:id', authorize, getUserSubscriptions);
 subscriptionRoutes.put('/:id/cancel',  (req, res) => res.send({title:'Cancel subscription'}));
 subscriptionRoutes.put('/upcoming-renewals',  (req, res) => res.send({title:'Get upcoming renewels'}));
 
