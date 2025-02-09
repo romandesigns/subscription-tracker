0da636116443cf4df67120cc7e081fd773b65752
Roman <wavystyledev@gmail.com>
2025-02-09 15:39:28 -0500

    added arcject to protect API against spam

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ import userRoutes from "./routes/user-routes.js";
 import connectToDB from "./database/mongodb.js";
 import errorMiddleware from "./middleware/error.middleware.js";
 import cookieParser from "cookie-parser";
+import arcjetMiddleware from "./middleware/arcjet-middleware.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users',userRoutes);
--- config/env.js
+++ config/env.js
@@ -1,4 +1,4 @@
 import {config} from 'dotenv';
 
 config({path: `.env.${process.env.NODE_ENV ||'development'}.local`});
-export const {PORT, NODE_ENV,DB_URI, JWT_EXPIRES_IN, JWT_SECRET} = process.env;
+export const {PORT, NODE_ENV,DB_URI, JWT_EXPIRES_IN, JWT_SECRET,ARCJET_ENV,ARCJET_KEY} = process.env;
