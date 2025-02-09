40929283e2f57b1d6b869ab7fe25a4bbd3729856
Roman <wavystyledev@gmail.com>
2025-02-09 13:53:12 -0500

    implementation of sign up endpoint

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ import subscriptionRoutes from "./routes/subscription-routes.js";
 import userRoutes from "./routes/user-routes.js";
 import connectToDB from "./database/mongodb.js";
 import errorMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users',userRoutes);
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,8 +1,9 @@
 import {Router} from 'express';
+import {signUp} from "../controllers/auth-controller.js";
 
 const authRouter = Router();
 
-authRouter.post('/sign-up', (req, res) => res.send({title:'Sign Up'}));
+authRouter.post('/sign-up', signUp);
 authRouter.post('/sign-in', (req, res) => res.send({title:'Sign In'}));
 authRouter.post('/sign-out', (req, res) => res.send({title:'Sign Out'}));
 
